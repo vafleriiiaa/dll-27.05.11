18d235f3f459df23abdf744e405b7e98bbe8f675
Валерия Майкова <vamaylik@gmail.com>
2024-09-03 16:24:51 +0300

    Добавьте файлы проекта.

---
+++ 27.05.11/dllmain.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <windows.h>
+// главная функция
+BOOL WINAPI DllMain(HINSTANCE hDll, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH: // загрузка DLL
+		break;
+	case DLL_THREAD_ATTACH: // создание потока
+		break;
+	case DLL_THREAD_DETACH: // завершение потока
+		break;
+	case DLL_PROCESS_DETACH: // отключение DLL
+		break;
+	}
+	return TRUE;
+}
+extern "C" _declspec(dllexport) int count = 0;
+extern "C" _declspec(dllexport) int Add(int n)
+{
+	count += n;
+	return count;
+}
+extern "C" _declspec(dllexport) int Sub(int n)
+{
+	count -= n;
+	return count;
+}
